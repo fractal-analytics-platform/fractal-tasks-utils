72e347dc8efb72fe94eeef76b1b36056d8555e5d
lorenzo <lorenzo.cerrone@uzh.ch>
2026-03-12 16:05:32 +0100

    adopt vcs version control

--- src/fractal_tasks_utils/__init__.py
+++ src/fractal_tasks_utils/__init__.py
@@ -1,1 +1,8 @@
 """This package contains general utilities to build fractal tasks."""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("fractal-tasks-utils")
+except PackageNotFoundError:
+    __version__ = "unknown"
